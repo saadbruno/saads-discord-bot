d601c42d5bf33b53a199bfae959fff4ef17c65c2
Bruno Saad <saadbruno@gmail.com>
2025-10-30 16:08:56 +0000

    moved event handler to separate files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require(`node:fs`);
 const path = require(`node:path`);
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require(`discord.js`);
+const { Client, Collection, Events, GatewayIntentBits } = require(`discord.js`);
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -33,32 +33,18 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+const eventsPath = path.join(__dirname, `events`);
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(`.js`));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
     }
-    try {
-        await command.execute(interaction);
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-    catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: `There was an error while executing this command!`,
-                flags: MessageFlags.Ephemeral,
-            });
-        }
-        else {
-            await interaction.reply({
-                content: `There was an error while executing this command!`,
-                flags: MessageFlags.Ephemeral,
-            });
-        }
-    }
-});
+}
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
